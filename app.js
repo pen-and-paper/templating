f3e8e08d82b4a1a1a1068bae49d9d8bc95732eab
herohamp <hampmoe@me.com>
2017-06-21 10:17:15 -0400

    Bases

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const base = fs.readFileSync("./client/base.html", 'utf8');
+const nav = fs.readFileSync("./client/nav.html", 'utf8');
 
 app.get('/', function(req, res) {
-    res.send(parse("./index.html"));
+    res.send(parse("index.html", false));
 })
+
+app.use(express.static('public'))
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
 })
 
-function parse(file) {
-    return fs.readFileSync(file, 'utf8');
+function parse(file, logged) {
+
+
+    var page = base;
+
+    if (logged) {
+        //Then we will parse for the logged in navbar
+    }
+    else {
+        page = page.replace("{{nav}}", nav);
+    }
+
+    page = page.replace("{{content}}", fs.readFileSync("./client/views/" + file, 'utf8'));
+
+    return page;
 }
