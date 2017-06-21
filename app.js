2598093650acb69fb23d7b6eba254120b2d72850
herohamp <hampmoe@me.com>
2017-06-21 17:03:02 +0000

    Better Routing and 404 page

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ const fs = require('fs');
 const app = express();
 const base = fs.readFileSync("./client/base.html", 'utf8');
 const nav = fs.readFileSync("./client/nav.html", 'utf8');
+const routes = JSON.parse(fs.readFileSync("./routes.json", 'utf8'));
 
-app.get('/', function(req, res) {
-    res.send(parse("index.html", false));
+for (var i in routes){
+    console.log(`${i} was loaded as a path!`);
+}
+
+app.use(express.static('public'));
+
+app.get('*', function(req, res) {
+    
+    if (!(req.path in routes)){
+        res.send(parse(routes["404"], false));
+    } else {
+        res.send(parse(routes[req.path], false));
+    }
 })
-
-app.use(express.static('public'))
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Example app listening on port '+process.env.PORT || "3000"+"!");
 })
 
-function parse(file, logged) {
+function parse(route, logged) {
 
 
     var page = base;
-
+    
+    page = page.replace("{{title}}", route.title);
+    
     if (logged) {
         //Then we will parse for the logged in navbar
     }
@@ -26,7 +38,7 @@ function parse(file, logged) {
         page = page.replace("{{nav}}", nav);
     }
 
-    page = page.replace("{{content}}", fs.readFileSync("./client/views/" + file, 'utf8'));
+    page = page.replace("{{content}}", fs.readFileSync("./client/views/" + route.file, 'utf8'));
 
     return page;
 }
